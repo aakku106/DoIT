6b7ddacfa3443983f83ee28448554afe245e995b
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-03-04 01:24:25 +0545

    using defer here was bad idea xd

--- internal/db/sqlite.go
+++ internal/db/sqlite.go
@@ -2,21 +2,19 @@ package db
 
 import (
 	"database/sql"
-	"github.com/aakku106/DoIT/internal/store"
 	_ "github.com/mattn/go-sqlite3"
-	"log"
 )
 
 func NewSQLite() (*sql.DB, error) {
-
 	db, err := sql.Open("sqlite3", "todo.db")
-
 	if err != nil {
-		log.Fatal(err)
 		return nil, err
 	}
-	defer db.Close()
-	queries := store.New(db)
-	log.Printf("queries: %v\n", queries)
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
 	return db, nil
 }
+
