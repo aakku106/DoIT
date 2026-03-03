1d509db8f73cb6c985ff9c3272c7022f231349e4
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-03-04 01:30:37 +0545

    creatign table todos

--- internal/db/sqlite.go
+++ internal/db/sqlite.go
@@ -15,6 +15,20 @@ func NewSQLite() (*sql.DB, error) {
 		return nil, err
 	}
 
+	schema := `
+	CREATE TABLE IF NOT EXISTS todos (
+		id INTEGER PRIMARY KEY,
+		session TEXT NOT NULL DEFAULT 'todo',
+		title TEXT NOT NULL,
+		created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		expires_at DATETIME,
+		completed INTEGER NOT NULL DEFAULT 0 CHECK (completed IN (0,1))
+	);`
+
+	if _, err := db.Exec(schema); err != nil {
+		return nil, err
+	}
+
 	return db, nil
 }
 
