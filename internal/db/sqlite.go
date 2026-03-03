98944a8b2a972144dd52aeb3638d30fae11f094e
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-03-04 00:57:38 +0545

    trying to properly form at sqlite.go fiel

--- internal/db/sqlite.go
+++ internal/db/sqlite.go
@@ -2,34 +2,23 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	"github.com/aakku106/DoIT/internal/store"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var DEBUG = true
-
 func NewSQLite() (*sql.DB, error) {
-	if DEBUG {
-		log.Println("Initilizating DB: ")
-	}
+
 	db, err := sql.Open("sqlite3", "todo.db")
-	queries := store.New(db)
+
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
 	}
 	defer db.Close()
-	if DEBUG {
-		log.Println("DB initilized")
-		return nil, err
-	}
-	if err := db.Ping(); err != nil {
-		log.Fatal(err)
-	}
+	queries := store.New(db)
+	fmt.Printf("queries: %v\n", queries)
 	return db, nil
 }
-
-// I have no idea how to run migration till date
-// func RunMigrations(db *sql.DB) error {}
