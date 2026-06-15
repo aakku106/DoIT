332bee8f0e83b74a3fde913708df33f10ef7de13
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-06-16 00:35:51 +0545

    called New from db.go of /store

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -6,11 +6,20 @@ import (
 
 	call "github.com/aakku106/DoIT/internal/cli"
 	create "github.com/aakku106/DoIT/internal/db"
+	"github.com/aakku106/DoIT/internal/store"
 )
 
 func main() {
 	args := os.Args
-	create.NewSQLite()
+	db, err := create.NewSQLite()
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	query := store.New(db)
+
 	switch args[1] {
 	case "add":
 		if len(args) < 3 {
@@ -18,11 +27,10 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Print("Adding:")
-		add(args)
+		call.AddTodo(query, args[2])
 
 	case "list":
 		fmt.Println("Listing data of todo")
-		list()
 
 	case "done":
 		fmt.Println(args[2], "task completed")
@@ -30,15 +38,15 @@ func main() {
 			fmt.Println("Specify what have you done ?")
 			os.Exit(1)
 		}
-		done(args)
 
 	case "remove":
 		if len(args) < 3 {
 			fmt.Println("Specify what to Remove ?")
 			os.Exit(1)
 		}
+
+		fmt.Println("***---Are you sure You want To remove: ", args[2])
 		fmt.Println("Removing: ", args[3])
-		remove(args)
 
 	default:
 		fmt.Println("Session call")
@@ -48,17 +56,6 @@ func main() {
 		}
 		sessionCall(args)
 	}
-}
-
-func add(args []string) {
-	call.AddTodo(args[2])
-}
-func list() {
-	call.ListTodos()
-}
-func done(args []string) {}
-func remove(args []string) {
-	fmt.Println("***---Are you sure You want To remove: ", args[2])
 }
 
 func sessionCall(args []string) {}
