55241aac3bd00341d4af3df43e36e6433ead644a
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-06-16 00:50:37 +0545

    Proper add to db

--- internal/cli/commands.go
+++ internal/cli/commands.go
@@ -1,13 +1,21 @@
 package cli
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+
 	"github.com/aakku106/DoIT/internal/store"
 )
 
 func AddTodo(query *store.Queries, title string) {
 	fmt.Printf("\t%s\n", title)
-
+	param := store.CreateTodoParams{
+		Title:     title,
+		Session:   "todo",
+		ExpiresAt: sql.NullTime{Valid: false},
+	}
+	query.CreateTodo(context.Background(), param)
 }
 func ListTodos() {
 	fmt.Println("Listing TODO:")
