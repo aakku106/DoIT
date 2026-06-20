e51e798eaabcd7696b11dbeb65a88691a19a62d2
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-06-21 02:50:42 +0545

    removig ambiguty

--- sql/queries.sql
+++ sql/queries.sql
@@ -5,21 +5,24 @@ RETURNING *;
 
 -- name: ListTodos :many
 SELECT id, title FROM todos
-WHERE session = ? AND completed = 0
+WHERE todos.session = ? AND completed = 0
 ORDER BY created_at DESC;
 
 -- name: ListTodoIDs :many
 SELECT id FROM todos
-WHERE session = ?
+WHERE todos.session = ?
 ORDER BY created_at DESC;
 
 -- name: CompleteTodoTransaction :exec
 INSERT INTO completed (session, title)
-SELECT session, title FROM todos WHERE id = ?;
+SELECT session, title FROM todos 
+WHERE todos.id = ?;
 
 -- name: TrashTodoTransaction :exec
 INSERT INTO trash (session, title, created_at)
-SELECT session, title, created_at FROM todos WHERE id = ?;
+SELECT session, title, created_at FROM todos 
+WHERE todos.id = ?;
 
 -- name: DeleteFromTodos :exec
-DELETE FROM todos WHERE id = ?;
+DELETE FROM todos 
+WHERE todos.id = ?;
