3e5910102313629d5d352efb497596a687f8c098
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-06-21 02:46:23 +0545

    new updated quries for updated articture schema

--- sql/queries.sql
+++ sql/queries.sql
@@ -13,11 +13,13 @@ SELECT id FROM todos
 WHERE session = ?
 ORDER BY created_at DESC;
 
--- name: CompleteTodo :exec
-UPDATE todos
-SET completed = 1
-WHERE id = ?;
+-- name: CompleteTodoTransaction :exec
+INSERT INTO completed (session, title)
+SELECT session, title FROM todos WHERE id = ?;
 
--- name: DeleteTodo :exec
-DELETE FROM todos
-WHERE id = ?;
+-- name: TrashTodoTransaction :exec
+INSERT INTO trash (session, title, created_at)
+SELECT session, title, created_at FROM todos WHERE id = ?;
+
+-- name: DeleteFromTodos :exec
+DELETE FROM todos WHERE id = ?;
