44ab69d928b238ce5cc7c6f70f00f1f84d1cb5e8
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-06-21 02:57:40 +0545

    usign alicing where possible for clearity

--- sql/queries.sql
+++ sql/queries.sql
@@ -4,24 +4,24 @@ VALUES (?, ?, ?)
 RETURNING *;
 
 -- name: ListTodos :many
-SELECT id, title FROM todos
-WHERE todos.session = ?
-ORDER BY created_at DESC;
+SELECT t.id, t.title FROM todos AS t
+WHERE t.session = ?
+ORDER BY t.created_at DESC;
 
 -- name: ListTodoIDs :many
-SELECT id FROM todos
-WHERE todos.session = ?
-ORDER BY created_at DESC;
+SELECT t.id FROM todos AS t
+WHERE t.session = ?
+ORDER BY t.created_at DESC;
 
 -- name: CompleteTodoTransaction :exec
 INSERT INTO completed (session, title)
-SELECT session, title FROM todos 
-WHERE todos.id = ?;
+SELECT t.session, t.title FROM todos AS t
+WHERE t.id = ?;
 
 -- name: TrashTodoTransaction :exec
 INSERT INTO trash (session, title, created_at)
-SELECT session, title, created_at FROM todos 
-WHERE todos.id = ?;
+SELECT t.session, t.title, t.created_at FROM todos AS t
+WHERE t.id = ?;
 
 -- name: DeleteFromTodos :exec
 DELETE FROM todos 
