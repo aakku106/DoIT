a29d4ca6d0b4d027d81f4d4dbfe0eac94e2aca31
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-06-25 12:55:18 +0545

    Conformatoin messae for Remove and Clear calls

--- cmd/todo/clear.go
+++ cmd/todo/clear.go
@@ -10,6 +10,7 @@ import (
 )
 
 func clearCompleted(q *store.Queries) {
+	fmt.Println(cli.Red, "___This Clear all Tasks From Completed, Task Cannot be Retrived Again___", cli.Reset)
 	fmt.Println(cli.Red, "---Are you sure You want To Clear Completed :", cli.Reset, "Y/N")
 	var a rune
 	fmt.Scanf("%c", &a)
@@ -21,12 +22,25 @@ func clearCompleted(q *store.Queries) {
 }
 
 func clearTrash(q *store.Queries) {
-	fmt.Println(cli.Red, "---Are you sure You want To Clear Completed :", cli.Reset, "Y/N")
+	fmt.Println(cli.Red, "___This Clear all Tasks From Trash, Task Cannot be Retrived Again___", cli.Reset)
+	fmt.Println(cli.Red, "---Are you sure You want To Clear Trash :", cli.Reset, "Y/N")
 	var a rune
 	fmt.Scanf("%c", &a)
 	if unicode.ToLower(a) == 'n' {
 		os.Exit(0)
 	} else {
 		cli.ClearTrash(q)
+	}
+}
+
+func clearIgnored(q *store.Queries) {
+	fmt.Println(cli.Red, "___This Clear all Tasks From Ignored, Task Cannot be Retrived Again___", cli.Reset)
+	fmt.Println(cli.Red, "---Are you sure You want To Clear Ignored :", cli.Reset, "Y/N")
+	var a rune
+	fmt.Scanf("%c", &a)
+	if unicode.ToLower(a) == 'n' {
+		os.Exit(0)
+	} else {
+		cli.ClearIgnored(q)
 	}
 }
