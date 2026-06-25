efdbfff785156eb47e0d303c546a2849967ffcfb
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-06-25 13:39:04 +0545

    all move commends decleration on /cmd/to

--- cmd/todo/move.go
+++ cmd/todo/move.go
@@ -1,1 +1,15 @@
 package main
+
+import "github.com/aakku106/DoIT/internal/store"
+
+func moveTrash(q *store.Queries, args []string)            {}
+func moveTrashToCompleted(q *store.Queries, args []string) {}
+func moveTrashToIgnored(q *store.Queries, args []string)   {}
+
+func moveComepeted(q *store.Queries, args []string)          {}
+func moveComepetedToTrash(q *store.Queries, args []string)   {}
+func moveComepetedToIgnored(q *store.Queries, args []string) {}
+
+func moveIgnored(q *store.Queries, args []string)          {}
+func moveIgnoredToTrash(q *store.Queries, args []string)   {}
+func moveIgnoredCompleted(q *store.Queries, args []string) {}
