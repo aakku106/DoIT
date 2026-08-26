1c2f8ba8c52198e664f77e1d434d4be81e2b7a91
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-08-26 11:06:33 +0545

    what to do next ?

--- cmd/todo/init.go
+++ cmd/todo/init.go
@@ -5,5 +5,6 @@ package main
 create the folder named .doit inside the parent dir
 now inside
 .doit/
-it needs to have doit.db
+it needs to have doit.db (This is our main db where everythign happens)
+That's all for now
 */
