2993f34d3955669e66ddf507cef75a876a4272cb
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-09-02 02:04:23 +0545

    proper error message

--- cmd/todo/clear.go
+++ cmd/todo/clear.go
@@ -10,37 +10,61 @@ import (
 )
 
 func clearCompleted(q *store.Queries) {
-	fmt.Println(cli.Red, "___This Clear all Tasks From Completed, Task Cannot be Retrived Again___", cli.Reset)
+	fmt.Println(cli.Red, "___This Clear all Tasks From Completed list, Task Cannot be Retrived Again !!!___", cli.Reset)
 	fmt.Println(cli.Red, "---Are you sure You want To Clear Completed :", cli.Reset, "Y/N")
 	var a rune
 	fmt.Scanf("%c", &a)
 	if unicode.ToLower(a) == 'n' {
 		os.Exit(0)
-	} else {
-		cli.ClearCompleted(q)
+	} else if a == 'Y' {
+		fmt.Println(cli.Red, "CONFIRN CLEARING COMPLETED LIST:", cli.Reset, "YeS NuKe/N")
+		var b string
+		fmt.Scanf("%s", &b)
+		if b == "YeS NuKe" {
+			cli.ClearCompleted(q)
+		}
+		fmt.Println("CLEARING COMPLETED LIST ABORTED !!!")
 	}
+	fmt.Println("You were supposed to select between Y and N")
+	os.Exit(1)
 }
 
 func clearTrash(q *store.Queries) {
-	fmt.Println(cli.Red, "___This Clear all Tasks From Trash, Task Cannot be Retrived Again___", cli.Reset)
+	fmt.Println(cli.Red, "___This Clear all Tasks From Trash list, Task Cannot be Retrived Again !!!___", cli.Reset)
 	fmt.Println(cli.Red, "---Are you sure You want To Clear Trash :", cli.Reset, "Y/N")
 	var a rune
 	fmt.Scanf("%c", &a)
 	if unicode.ToLower(a) == 'n' {
 		os.Exit(0)
-	} else {
-		cli.ClearTrash(q)
+	} else if a == 'Y' {
+		fmt.Println(cli.Red, "CONFIRN CLEARING TRASH LIST:", cli.Reset, "YeS NuKe/N")
+		var b string
+		fmt.Scanf("%s", &b)
+		if b == "YeS NuKe" {
+			cli.ClearCompleted(q)
+		}
+		fmt.Println("CLEARING TRASH LIST ABORTED !!!")
 	}
+	fmt.Println("You were supposed to select between Y and N")
+	os.Exit(1)
 }
 
 func clearIgnored(q *store.Queries) {
-	fmt.Println(cli.Red, "___This Clear all Tasks From Ignored, Task Cannot be Retrived Again___", cli.Reset)
+	fmt.Println(cli.Red, "___This Clear all Tasks From Ignored list, Task Cannot be Retrived Again !!!___", cli.Reset)
 	fmt.Println(cli.Red, "---Are you sure You want To Clear Ignored :", cli.Reset, "Y/N")
 	var a rune
 	fmt.Scanf("%c", &a)
 	if unicode.ToLower(a) == 'n' {
 		os.Exit(0)
-	} else {
-		cli.ClearIgnored(q)
+	} else if a == 'Y' {
+		fmt.Println(cli.Red, "CONFIRN CLEARING IGNORED LIST:", cli.Reset, "YeS NuKe/N")
+		var b string
+		fmt.Scanf("%s", &b)
+		if b == "YeS NuKe" {
+			cli.ClearCompleted(q)
+		}
+		fmt.Println("CLEARING IGNORED LIST ABORTED !!!")
 	}
+	fmt.Println("You were supposed to select between Y and N")
+	os.Exit(1)
 }
