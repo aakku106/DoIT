09d31b11867f081f17916c8eec6dce9f9db5a78b
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-09-02 01:48:44 +0545

    standarizing

--- cmd/todo/add.go
+++ cmd/todo/add.go
@@ -10,7 +10,7 @@ import (
 
 func add(q *store.Queries, args []string) {
 	if len(args) < 3 || len(args) != 3 {
-		fmt.Println(call.Cyan, "Specify what to add !, run exactly doit add your task", call.Reset)
+		fmt.Println(call.Cyan, "Specify what to add !, run exactly doit <a/add> your task", call.Reset)
 		os.Exit(1)
 	}
 	fmt.Print("Adding:")
