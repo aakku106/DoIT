44451968e667342dbf7f49efefe31eed55525ae0
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-09-02 15:49:00 +0545

    proper len checks

--- cmd/todo/add.go
+++ cmd/todo/add.go
@@ -9,10 +9,12 @@ import (
 )
 
 func add(q *store.Queries, args []string) {
-	if len(args) < 3 || len(args) != 3 {
+	if len(args) < 3 {
 		fmt.Println(call.Cyan, "Specify what to add !, run exactly doit <a/add> your task", call.Reset)
 		os.Exit(1)
 	}
 	fmt.Print("Adding:")
-	call.AddTodo(q, args[2])
+	if len(args) == 3 {
+		call.AddTodo(q, args[2])
+	}
 }
