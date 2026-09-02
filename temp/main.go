119427631981bdbd99caae68933df955b0ce33d3
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-09-02 16:17:26 +0545

    packing multiple added tasks into one slice

--- temp/main.go
+++ temp/main.go
@@ -14,22 +14,30 @@ func main() {
 	if len(args) < 3 {
 		os.Exit(106)
 	}
+
 	fmt.Println("Addign Multiple tasks to todo list")
-	i := 0 // just to know hou many times loop ran
+
+	i := 0
 	commaCount := 0
+	var newTaskList []string
+
 	for _, v := range args[2:] {
 		if v == "," {
 			commaCount++
 		}
+
 		if v != "," {
 			if v[0] != '-' {
-				fmt.Printf("\nIndex:%d\t\tvalue: %s\n", i, v)
+				newTaskList = append(newTaskList, v)
 				i++
-
 			}
 		}
+
 	}
-	if i == commaCount+1 {
-		fmt.Printf("\nAdding:%d\ttaks to todoList\n", i)
+	if i != commaCount+1 {
+		os.Exit(106)
 	}
+	fmt.Printf("\nAdding:%d\ttaks to todoList\n", i)
+	fmt.Printf("\ntodoList:%v,\tLen:%d,\tCap:%d", newTaskList, len(newTaskList), cap(newTaskList))
+
 }
