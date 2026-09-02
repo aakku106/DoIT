81c93bbd9cbef6414e008b8663e4b5609d12ffa5
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-09-02 15:50:48 +0545

    logical cleanup

--- temp/main.go
+++ temp/main.go
@@ -8,15 +8,16 @@ import (
 func main() {
 	args := os.Args
 	// fmt.Printf("Args: %v \nLength of arg: %d ", args, len(args))
-	if args[1] == "add" {
-		if len(args) > 3 {
-			fmt.Println("Addign Multiple tasks to todo list")
-			i := 1 // just to know hou many times loop ran
-			for _, v := range args[2:] {
-				if v != "," {
-					fmt.Printf("\nIndex:%d\tvalue: %s\n", i, v)
-					i++
-				}
+	if args[1] != "add" {
+		os.Exit(106)
+	}
+	if len(args) > 3 {
+		fmt.Println("Addign Multiple tasks to todo list")
+		i := 1 // just to know hou many times loop ran
+		for _, v := range args[2:] {
+			if v != "," {
+				fmt.Printf("\nIndex:%d\tvalue: %s\n", i, v)
+				i++
 			}
 		}
 	}
