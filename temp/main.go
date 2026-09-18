fcd1ebcb04c01dc9fd0f82759aee399b868b553f
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-09-18 16:20:44 +0545

    final cleanup + comment fixing

--- temp/main.go
+++ temp/main.go
@@ -28,7 +28,6 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Println(val)
 	fmt.Printf("\ntodoList:%v,\tLen:%d,\tCap:%d\n", val, len(val), cap(val))
 }
 
@@ -36,10 +35,11 @@ func extractMultipleTasks(args []string) ([][]string, error) {
 
 	rawArgs := args[2:]
 
-	// Bucked for currentTask and Group of Current Task
+	// Bucket for currentTask and Group of Current Task
 	taskGroups := make([][]string, 0, len(rawArgs)/2)
 	currentGroup := make([]string, 0, 2) // cap=2 cause, for now we only have 2 possible entrires{task,time}
 
+	// Group arguments delimited by ","
 	for _, val := range rawArgs {
 		if val == "," {
 			if len(currentGroup) == 0 {
@@ -51,11 +51,12 @@ func extractMultipleTasks(args []string) ([][]string, error) {
 			currentGroup = append(currentGroup, val)
 		}
 	}
+
 	if len(currentGroup) == 0 {
 		return nil, fmt.Errorf("comma in last without any task.")
 	}
+	// pushing final task in group bucket
 	taskGroups = append(taskGroups, currentGroup)
 
 	return taskGroups, nil
-
 }
