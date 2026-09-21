bad945934de3e520dfaca075bda7b4b426aafce6
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-09-21 22:49:02 +0545

    varbose flag

--- cmd/todo/add.go
+++ cmd/todo/add.go
@@ -15,6 +15,8 @@ type Task struct {
 	Time  string
 }
 
+const DeadMissingWarningMessage bool = false
+
 func add(q *store.Queries, args []string) {
 	// if len(args) < 3 {
 	// 	fmt.Println(call.Cyan, "Specify what to add !, run exactly doit <a/add> your task", call.Reset)
