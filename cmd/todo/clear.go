b448098469ba391cf858edd6b882ceb85b48c46f
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-09-22 15:19:16 +0545

    proper nuke function for todos table

--- cmd/todo/clear.go
+++ cmd/todo/clear.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 	"unicode"
 
 	"github.com/aakku106/DoIT/internal/cli"
@@ -11,26 +13,49 @@ import (
 
 // TODO: Fix logic here
 func clearTodo(q *store.Queries) {
-	fmt.Println(cli.Red, "___This Clear all Tasks From Todo list, Task Cannot be Retrived Again !!!___", cli.Reset)
-	fmt.Println(cli.Red, "---Are you sure You want To Clear Todo :", cli.Reset, "Y/N")
-	var a rune
-	fmt.Scanf("%c", &a)
-	if unicode.ToLower(a) == 'n' {
-		os.Exit(0)
-	} else if a == 'Y' {
-		fmt.Println(cli.Red, "CONFIRN CLEARING TODO LIST:", cli.Reset, "YeS_NuKe / N")
-		var b string
-		fmt.Scanf("%s", &b)
-		if b == "n" || b == "N" {
-			fmt.Println("CLEARING TODO LIST ABORTED !!!")
-		} else if b == "YeS_NuKe" {
-			cli.ClearTodos(q)
-		} else {
-			fmt.Println("CLEARING TODO LIST ABORTED !!!")
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println(cli.Red, "___This clears all tasks from Todo list. Tasks CANNOT be retrieved again!___", cli.Reset)
+	fmt.Print(cli.Red, "---Are you sure you want to clear Todo (Y/N)? ", cli.Reset)
+
+	// Read first input line
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	input = strings.TrimSpace(input)
+	if len(input) == 0 {
+		fmt.Println("Invalid selection.")
+		return
+	}
+
+	// 'n' or 'N' cancels the operation
+	if strings.ToLower(input) == "n" {
+		fmt.Println("Operation cancelled.")
+		return
+	} else if input == "Y" { // Strictly checks for uppercase 'Y'
+		fmt.Println(cli.Red, "CONFIRM CLEARING TODO LIST:", cli.Reset, "Type 'YeS NuKe ToDos' to confirm:")
+
+		// Read second input line (with spaces)
+		confirmInput, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading confirmation:", err)
+			return
 		}
+
+		confirmInput = strings.TrimSpace(confirmInput)
+
+		if confirmInput == "YeS NuKe ToDos" {
+			cli.ClearTodos(q)
+			fmt.Println("Todo list successfully cleared!")
+			return
+		}
+
+		fmt.Println("CLEARING TODO LIST ABORTED !!!")
 	} else {
-		fmt.Println("You were supposed to select between Y and N")
-		os.Exit(1)
+		fmt.Println("You were supposed to select between uppercase 'Y' and 'N'/'n'")
 	}
 }
 
